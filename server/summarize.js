e154b9338119b2569d84e2ebd75ba522ddc25889
Ricardo Jeferson da Silva Francisco <122450316+totorourbem@users.noreply.github.com>
2023-09-15 16:15:36 -0300

    Arquivos uppados, falta fazer read.me
    Projeto inteiro feito na NLW da RocketSeat, modulo Foundations

---
+++ server/summarize.js
@@ -0,0 +1,26 @@
+import {pipeline} from '@Xenova/transformers'
+import { summaryExample } from './utils/summary.js'
+
+
+export async function summarize(text) {
+    try {
+        //return summaryExample
+
+        console.log('Realizando resumo')
+
+        const generator = await pipeline(
+            'summarization', 
+            'Xenova/distilbart-cnn-12-6'
+        )
+
+        const output = await generator(text)
+        console.log('Resumo concluído com sucesso')
+        return output [0].summary_text
+
+    } catch (error) {
+
+        console.log('Não foi possível realizar o resumo', error)
+        throw new Error(error)
+        
+    }
+}
---
+++ web/main.js
@@ -0,0 +1,7 @@
+// JS scripts
+import "./form.js"
+
+//CSS styles
+import "./styles/base.css"
+import "./styles/app.css"
+import "./styles/form.css"
